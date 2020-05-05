a9e0523170f8838ea0192dc00ca75be8e6682b4c
Olivier Giniaux <olivier.giniaux@pi.xyz>
2020-05-05 10:35:32 +0200

    Fix create normals

--- Nanomesh/Algo/NormalsCreator.cs
+++ Nanomesh/Algo/NormalsCreator.cs
@@ -1,14 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Nanolabo
 {
     public class NormalsModifier
     {
+		public struct PosAndAttribute : IEquatable<PosAndAttribute>
+		{
+			public int position;
+			public Attribute attribute;
+
+			public override int GetHashCode()
+			{
+				return position.GetHashCode() ^ (attribute.GetHashCode() << 2);
+			}
+
+			public bool Equals(PosAndAttribute other)
+			{
+				return position == other.position && attribute.Equals(other.attribute);
+			}
+		}
+
 		public void Run(ConnectedMesh mesh, float smoothingAngle)
         {
+			float cosineThreshold = MathF.Cos(smoothingAngle * MathF.PI / 180f);
+
 			int[] positionToNode = mesh.GetPositionToNode();
-			Dictionary<Attribute, int> attributeToIndex = new Dictionary<Attribute, int>();
+
+			Dictionary<PosAndAttribute, int> attributeToIndex = new Dictionary<PosAndAttribute, int>();
+
+			Vector3F getFaceNormal(int nodeIndex)
+			{
+				int posA = mesh.nodes[nodeIndex].position;
+				int posB = mesh.nodes[mesh.nodes[nodeIndex].relative].position;
+				int posC = mesh.nodes[mesh.nodes[mesh.nodes[nodeIndex].relative].relative].position;
+
+				Vector3F faceNormal = Vector3.Cross(
+					mesh.positions[posB] - mesh.positions[posA],
+					mesh.positions[posC] - mesh.positions[posA]);
+
+				faceNormal.Normalize();
+
+				return faceNormal;
+			}
 
 			for (int p = 0; p < positionToNode.Length; p++)
 			{
@@ -18,67 +53,49 @@ namespace Nanolabo
 
 				Debug.Assert(!mesh.nodes[nodeIndex].IsRemoved);
 
-				Vector3F meanNormal = Vector3F.Zero;
-
-				// Computes mean normal at position
-				int sibling = nodeIndex;
+				int sibling1 = nodeIndex;
 				do
 				{
-					int posA = mesh.nodes[sibling].position;
-					int posB = mesh.nodes[mesh.nodes[sibling].relative].position;
-					int posC = mesh.nodes[mesh.nodes[mesh.nodes[sibling].relative].relative].position;
+					Vector3F sum = Vector3F.Zero;
 
-					Vector3F faceNormal = Vector3.Cross(
-						mesh.positions[posB] - mesh.positions[posA],
-						mesh.positions[posC] - mesh.positions[posA]);
-
-					faceNormal.Normalize();
-
-					meanNormal += faceNormal;
-
-				} while ((sibling = mesh.nodes[sibling].sibling) != nodeIndex);
-
-				meanNormal.Normalize();
-
-				sibling = nodeIndex;
-				do
-				{
-					int posA = mesh.nodes[sibling].position;
-					int posB = mesh.nodes[mesh.nodes[sibling].relative].position;
-					int posC = mesh.nodes[mesh.nodes[mesh.nodes[sibling].relative].relative].position;
-
-					Vector3F faceNormal = Vector3.Cross(
-						mesh.positions[posB] - mesh.positions[posA],
-						mesh.positions[posC] - mesh.positions[posA]);
-
-					faceNormal.Normalize();
-
-					float angle = Vector3F.AngleDegrees(meanNormal, faceNormal);
-
-					Attribute attribute = mesh.attributes[mesh.nodes[sibling].attribute];
-
-					if (angle  * 2f > smoothingAngle)
+					int sibling2 = nodeIndex;
+					do
 					{
-						attribute.normal = faceNormal;
-					}
-					else
-					{
-						attribute.normal = meanNormal;
-					}
+						if (sibling1 == sibling2)
+						{
+							sum += getFaceNormal(sibling2);
+						}
+						else
+						{
+							float dot = Vector3F.Dot(getFaceNormal(sibling1), getFaceNormal(sibling2));
 
-					// We only create a new attribute if it is entirely different
-					attributeToIndex.TryAdd(attribute, attributeToIndex.Count);
+							if (dot >= cosineThreshold)
+							{
+								sum += getFaceNormal(sibling2);
+							}
+						}
 
-					mesh.nodes[sibling].attribute = attributeToIndex[attribute];
+					} while ((sibling2 = mesh.nodes[sibling2].sibling) != nodeIndex);
 
-				} while ((sibling = mesh.nodes[sibling].sibling) != nodeIndex);
+					sum.Normalize();
+
+					Attribute attribute = mesh.attributes[mesh.nodes[sibling1].attribute];
+					attribute.normal = sum;
+
+					PosAndAttribute posAndAttribute = new PosAndAttribute { position = p, attribute = attribute };
+
+					attributeToIndex.TryAdd(posAndAttribute, attributeToIndex.Count);
+
+					mesh.nodes[sibling1].attribute = attributeToIndex[posAndAttribute];
+
+				} while ((sibling1 = mesh.nodes[sibling1].sibling) != nodeIndex);
 			}
 
 			// Assign new attributes
 			mesh.attributes = new Attribute[attributeToIndex.Count];
 			foreach (var pair in attributeToIndex)
 			{
-				mesh.attributes[pair.Value] = pair.Key;
+				mesh.attributes[pair.Value] = pair.Key.attribute;
 			}
 		}
 	}
