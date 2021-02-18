8dc88e78ed71a809b72d76d57a87560d98760ebd
Olivier Giniaux <oginiaux@gmail.com>
2021-02-18 21:22:14 +0100

    Fix sandbox path on macos

--- Nanomesh.Sandbox/Program.cs
+++ Nanomesh.Sandbox/Program.cs
@@ -18,7 +18,7 @@ namespace Nanomesh.Sandbox
 
         static void DecimateFile()
         {
-            SharedMesh sharedMesh = ImporterOBJ.Read(@"..\..\..\..\Nanomesh.Tests\test-models\buggy.obj");
+            SharedMesh sharedMesh = ImporterOBJ.Read(@"../../../../Nanomesh.Tests/test-models/buggy.obj");
             //sharedMesh.groups = new Group[3]
             //{
             //    new Group { firstIndex = 0, indexCount = 9000 },
@@ -48,8 +48,8 @@ namespace Nanomesh.Sandbox
 
             Console.WriteLine("Polycount : " + mesh.FaceCount);
 
-            Directory.CreateDirectory(@"..\..\..\..\Nanomesh.Tests\output\");
-            ExporterOBJ.Save(mesh.ToSharedMesh(), @"..\..\..\..\Nanomesh.Tests\output\decimation.obj");
+            Directory.CreateDirectory(@"../../../../Nanomesh.Tests/output/");
+            ExporterOBJ.Save(mesh.ToSharedMesh(), @"../../../../Nanomesh.Tests/output/decimation.obj");
         }
 
         static void Benchmark()
