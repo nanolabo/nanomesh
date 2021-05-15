a1bfe06b9877c14b2bd14ea62c4bc5a6d31243f4
Olivier Giniaux <oginiaux@gmail.com>
2021-05-15 16:08:39 +0200

    Add custom id to AttributeDefinition

--- Nanomesh/Mesh/Attributes/AttributeDefinition.cs
+++ Nanomesh/Mesh/Attributes/AttributeDefinition.cs
@@ -4,17 +4,27 @@
     {
         public double weight;
         public AttributeType type;
+        public int id;
 
         public AttributeDefinition(AttributeType type)
         {
-            weight = 1;
+            this.weight = 1;
             this.type = type;
+            this.id = 0;
         }
 
         public AttributeDefinition(AttributeType type, double weight)
         {
             this.weight = weight;
             this.type = type;
+            this.id = 0;
+        }
+
+        public AttributeDefinition(AttributeType type, double weight, int id)
+        {
+            this.weight = weight;
+            this.type = type;
+            this.id = id;
         }
     }
 }
